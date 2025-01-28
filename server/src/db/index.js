6a3fb07eef24930138265c2decdec9c76331feaf
Himanshu <himanshus@newgenit.com>
2025-01-28 18:19:41 +0530

    setup

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -4,6 +4,7 @@ import { DB_NAME } from "../constants.js";
 const dbConnect = async () => {
     try {
         mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`MongoDB Connected!`);
     } catch (error) {
         console.log("MongoDB Connection Failed", error);
         process.exit(1);
